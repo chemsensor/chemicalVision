295c3088911dcb1825f8d0bbd4186808af174f35
Kevin Cantrell <cantrell@up.edu>
2025-10-03 17:24:40 -0700

    began calc of euclidian distance

--- colorMatchPostProcessing.py
+++ colorMatchPostProcessing.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 26 16:51:06 2025
-
-@author: kevin
-"""
 import matplotlib.pyplot as plt
+import numpy as np
 
+def EuclidianDistance(cc1,cc2):
+    coordinates=len(cc1)
+    totalDistance=0
+    for axis in range(coordinates):
+        distance=cc1[axis]-cc2[axis]
+        totalDistance=totalDistance+distance**2
+    totatalDistance=np.sqrt(totalDistance)
+    return totatalDistance
+    
+        
 pads=standardSwatchStats[14,0,0,:]
 pHs=standardSwatchStats[13,0,0,:]
 padList=set(pads)
@@ -17,4 +21,24 @@ for padNumber in padList:
         axes[int(padNumber),col].plot(pHs[padMask],standardSwatchStats[cc,0,0,:][padMask],'-or')
         axes[int(padNumber),col].plot(pHs[padMask],standardSwatchStats[cc+1,0,0,:][padMask],'-og')
         axes[int(padNumber),col].plot(pHs[padMask],standardSwatchStats[cc+2,0,0,:][padMask],'-ob')
-    
+
+#for Lab color space initial cc is 6
+refLAB=standardSwatchStats[6:9,0,0,:]
+samLAB=parameterStats[6:9,0,0,0:3]
+pads=standardSwatchStats[14,0,0,:]
+numPads=samLAB.shape[1]
+numRefsPads=refLAB.shape[1]
+numRefs=int(numRefsPads/numPads)
+distances=np.zeros((numRefs))
+pHrefs=np.zeros((numRefs))
+for pad in range(numPads):
+    cc1=samLAB[:,pad]
+    padMask=pad==pads
+    refSamePad=refLAB[:,padMask]
+    for ref in range(numRefs):
+        cc2=refSamePad[:,ref]
+        distances[ref]=distances[ref]+EuclidianDistance(cc1,cc2)
+        pHrefs[ref]=pHs[padMask][ref]
+fig,ax=plt.subplots()
+ax.plot(pHrefs,distances)
+
