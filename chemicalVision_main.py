bf30c1b1d9dbe36f3ea6befe8b27603cffefe8c9
Kevin Cantrell <cantrell@up.edu>
2026-02-20 17:36:44 -0800

    pH estimation in the main script

--- chemicalVision_main.py
+++ chemicalVision_main.py
@@ -12,6 +12,7 @@ import os
 import time
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 #requires: pip install opencv-python
 import xlsxwriter
 import cv2
@@ -192,6 +193,15 @@ else:
     templateHistogram=np.zeros((3,256))
         
 print('Running, press "q" to quit')
+
+def EuclidianDistance(cc1,cc2):
+    coordinates=len(cc1)
+    totalDistance=0
+    for axis in range(coordinates):
+        distance=cc1[axis]-cc2[axis]
+        totalDistance=totalDistance+distance**2
+    totatalDistance=np.sqrt(totalDistance)
+    return totatalDistance
 
 def FindLargestContour(mask):
     if float(float(cv2.__version__[0])+float(cv2.__version__[2])/10)>=4:
@@ -1636,6 +1646,45 @@ if dictSet['flg hb'][0]==2:
         dfTargetCDFs = pd.DataFrame({'BlueHist':tgt_cdfB, 'GreenHist':tgt_cdfG, 'RedHist':tgt_cdfR})
         dfTargetCDFs.to_csv(data_file_path, index=False)
 
+for startIndex,endIndex in zip([0,60],[60,124]):       
+    pads=standardSwatchStats[14,0,0,startIndex:endIndex]
+    pHs=standardSwatchStats[13,0,0,startIndex:endIndex]
+    padList=set(pads)
+    samLAB=parameterStats[7:9,0,0,0:4]
+    fig,axes=plt.subplots(len(padList),1,sharex=True,sharey=True)
+    cc=7
+    for padNumber in padList:
+        padMask=padNumber==standardSwatchStats[14,0,0,startIndex:endIndex]
+        axes[int(padNumber)].plot(pHs[padMask],standardSwatchStats[cc,0,0,startIndex:endIndex][padMask],'-ok')
+        axes[int(padNumber)].plot(pHs[padMask],standardSwatchStats[cc+1,0,0,startIndex:endIndex][padMask],'-om')
+        #axes[int(padNumber)].plot(pHs[padMask],standardSwatchStats[cc+2,0,0,startIndex:endIndex][padMask],'-oy')
+        axes[int(padNumber)].plot([0,14],[samLAB[0,int(padNumber)],samLAB[0,int(padNumber)]],':k')
+        axes[int(padNumber)].plot([0,14],[samLAB[1,int(padNumber)],samLAB[1,int(padNumber)]],':m')
+        #axes[int(padNumber)].plot([0,14],[samLAB[2,int(padNumber)],samLAB[2,int(padNumber)]],':y')
+    
+    
+    #for LAB color space initial cc is 6
+    refLAB=standardSwatchStats[7:9,0,0,startIndex:endIndex]
+    samLAB=parameterStats[7:9,0,0,0:4]
+    pads=standardSwatchStats[14,0,0,startIndex:endIndex]
+    numPads=samLAB.shape[1]
+    numRefsPads=refLAB.shape[1]
+    numRefs=int(numRefsPads/numPads)
+    distances=np.zeros((numRefs))
+    pHrefs=np.zeros((numRefs))
+    for pad in range(numPads):
+        cc1=samLAB[:,pad]
+        padMask=pad==pads
+        refSamePad=refLAB[:,padMask]
+        for ref in range(numRefs):
+            cc2=refSamePad[:,ref]
+            distances[ref]=distances[ref]+EuclidianDistance(cc1,cc2)
+            pHrefs[ref]=pHs[padMask][ref]
+    fig,ax=plt.subplots()
+    ax.plot(pHrefs,distances)
+    
+    closestIndex=np.argmin(distances)
+    closest_pH=pHrefs[closestIndex]
 # dropSignal=parameterStats[dictSet['CNT yc'][0],dictSet['CNT yc'][1],0:frameIndex,dictSet['CNT yc'][2]]
 # boolDrop=da.hyst(dropSignal, dictSet['CNT hy'][0], dictSet['CNT hy'][1])
 # times,frames=da.crossBoolean(parameterStats[dictSet['CNT xc'][0],dictSet['CNT xc'][1],0:frameIndex,dictSet['CNT xc'][2]], boolDrop, crossPoint=0.5, direction='rising')
