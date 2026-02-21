08f8969fcc687d316914b00e32616e653718d904
Kevin Cantrell <cantrell@up.edu>
2026-02-20 17:31:19 -0800

    update box limits

--- colorMatchPostProcessing.py
+++ colorMatchPostProcessing.py
@@ -49,8 +49,40 @@ for startIndex,endIndex in zip([0,60],[60,124]):
     fig,ax=plt.subplots()
     ax.plot(pHrefs,distances)
     
-    # closestIndex=np.argmin(distances)
-    # closest_pH=pHrefs[closestIndex]
+    closestIndex=np.argmin(distances)
+    closest_pH=pHrefs[closestIndex]
+    
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+import numpy as np
+
+channel=1
+EuclidDist=np.zeros((4,100,2))
+intPh=np.zeros((4,100,2))
+DAg=np.zeros((100,2))
+Dstk=np.zeros((200))
+if (ClosestIndex-1)>=0 and (ClosestIndex+1)<=14:
+    for std in range(2):
+        for travel in range(100):
+            for pad in range(4):
+                    X0=UnknownData[pad,channel:channel+3]
+                    X1=CalibrationData[ClosestIndex-1+std,pad,channel:channel+3]
+                    X2=CalibrationData[ClosestIndex+std,pad,channel:channel+3]
+                    #tu=-numpy.dot(X1-X0,X2-X1)/numpy.absolute(numpy.dot(X2-X1,X2-X1))
+                    tu=travel/100.
+                    X3=X1+((X2-X1)*tu)
+                    EuclidDist[pad,travel,std]=numpy.linalg.norm(X3-X0)
+                    intPh[pad,travel,std]=CalibrationData[ClosestIndex-1+std,0,0]+(CalibrationData[ClosestIndex+std,0,0]-CalibrationData[ClosestIndex-1+std,0,0])*tu
+            DAg[:,std]=np.sum(EuclidDist,0)[:,std]   
+        Dstk[0+(std*100):100+(std*100)]=np.sum(EuclidDist,0)[:,std]
+        #plot(arange(100)+(100*std),DAg[:,std])
+        #pInt=(CalibrationData[ClosestIndex+std,0,0]-CalibrationData[ClosestIndex-1+std,0,0])*np.argmin(DAg[:,std])/100.0+CalibrationData[ClosestIndex-1+std,0,0]
+        #print "Best="+str(pInt)+"(d="+str(np.amin(DAg[:,std]))+") from "+str(CalibrationData[ClosestIndex-1+std,0,0])+" to "+str(CalibrationData[ClosestIndex+std,0,0])
+    pGlob=(CalibrationData[ClosestIndex+1,0,0]-CalibrationData[ClosestIndex-1,0,0])*np.argmin(Dstk)/200.0+CalibrationData[ClosestIndex-1,0,0]
+    pGlobED=np.amin(Dstk)
+    #print "Best="+str(pGlob)+"(d="+str(np.amin(Dstk))+") from "+str(CalibrationData[ClosestIndex-1,0,0])+" to "+str(CalibrationData[ClosestIndex+1,0,0])    
+    
     # #for RGB color space initial cc is 0
     # refLAB=standardSwatchStats[0:3,0,0,startIndex:endIndex]
     # samLAB=parameterStats[0:3,0,0,0:4]
@@ -69,3 +101,4 @@ for startIndex,endIndex in zip([0,60],[60,124]):
     #         distances[ref]=distances[ref]+EuclidianDistance(cc1,cc2)
     #         pHrefs[ref]=pHs[padMask][ref]
     # ax.plot(pHrefs,distances)
+"""
